48cd2347d2274ace530e76bfae35027296216179
Abdias Labrador LAptop windows10 asus <abdiaslabrador@gmail.com>
2020-05-26 02:08:52 -0400

    Procesos tomando en cuenta a los hijos listo

--- Script.ps1
+++ Script.ps1
@@ -23,7 +23,7 @@ $texbox.ScrollBars = "Vertical"
 $main_form.Controls.Add($texbox)
 #--------------------------------------------------------
 
-$global:MODE=0;
+$global:MODE=0
 $global:CURPID=0;
 
 #Botones 
@@ -39,9 +39,10 @@ $Buttoncpu = New-Object System.Windows.Forms.Button
 $Buttoncpu.Location = New-Object System.Drawing.Size(140,10)
 $Buttoncpu.Size = New-Object System.Drawing.Size(120,23)
 $Buttoncpu.Text = "CPU > 10%"
-$main_form.Controls.Add($Buttoncpu)
-$Buttoncpu.Add_Click({$global:MODE=1})
 
+$main_form.Controls.Add($Buttoncpu)
+
+$Buttoncpu.Add_Click({$global:MODE=1})
 $Buttonmem = New-Object System.Windows.Forms.Button
 $Buttonmem.Location = New-Object System.Drawing.Size(270,10)
 $Buttonmem.Size = New-Object System.Drawing.Size(120,23)
@@ -79,7 +80,7 @@ $global:ArrayList = New-Object System.Collections.ArrayList
 $global:pwshv = ((Get-Host).Version.Major)
 $global:rambyte =((Get-WmiObject Win32_ComputerSystem).totalphysicalmemory)
 
-Function GET_STAMP
+function GET_STAMP
 {
     $gp = gps | ? {$_.mainwindowtitle.length -ne 0} | where-object {$nombres_de_windows -notcontains $_.ProcessName}
 
@@ -114,21 +115,11 @@ Function GET_STAMP
     }
 
     $w= $gp | foreach-object{ 
-    	if($_.Name -eq "notepad")
-       {
-            $c=10.0; 
-            $r=10.0;
-       }
-        else
-       {
-            $c=1.0;
-            $r=1.0;
-       }
         $tmp=@{
-            PID=$_.Id;
-            Nombre=$_.Name;
-            RAM= $c;#[math]::round((($_.WorkingSetPrivate/$global:rambyte)*100.0),3);
-            CPU= $r;#$_.PercentProcessorTime;
+            PID=    $programs[$_.ProcessName].id;
+            Nombre= $programs[$_.ProcessName].name;
+            RAM=    [math]::round((($programs[$_.ProcessName].memory/$global:rambyte)*100.0),3);
+            CPU=    $programs[$_.ProcessName].processor;
         }
         New-Object -TypeName PSObject -prop $tmp;
     }  
@@ -154,27 +145,26 @@ Function Info
     }
     elseif($numero -eq 1)
     {  
-        return (GET_STAMP | where-object {$_.CPU -gt 2.0})
+        return (get_stamp | where-object {$_.cpu -gt 10})
     }
     elseif($numero -eq 2)
     {  
-        return (GET_STAMP | where-object {$_.RAM -gt 2.0})
+        return (get_stamp | where-object {$_.ram -gt 8})
     }
-    else
-    {
-	return (GET_STAMP | where-object {$_.RAM -gt 8.0 -and $_.CPU -gt 10.0})
+    elseif($numero -eq 3)
+    {  
+        return (get_stamp | where-object {$_.ram -gt 8 -and $_.CPU -gt 10})
     }
     
 }
-
 
 Function GET_DATA
 {   #Boton selector
     param($mode)
     $Object = $null
-    if($mode -eq 0){ $Object = (Info $mode); write-host $Object; return  $Object}  #todos los procesos
-    if($mode -eq 1){ $Object = (Info $mode); write-host $Object; return  $Object}  #procesos cpu con con uso de 10%cpu
-    if($mode -eq 2){ $Object = (Info $mode); write-host $Object; return  $Object}  #procesos memoria con consumo de 8%ram
+    if($mode -eq 0){ $Object = (Info $mode);  return  $Object}  #todos los procesos
+    if($mode -eq 1){ $Object = (Info $mode); write-host "este es object " $Object; return $Object} #procesos cpu con con uso de 10%cpu
+    if($mode -eq 2){ $Object = (Info $mode); write-host "este es object " $Object; return $Object}  #procesos memoria con consumo de 8%ram
     if($mode -eq 3){ $Object = (Stop(Info $mode)); return $Object} #eliminar los procesos con uso de 10%cpu y 8%ram del computador
 }
 
@@ -192,11 +182,28 @@ $DATA = GET_DATA($global:MODE)
 Function Update()
 {
    $DATA = GET_DATA($global:MODE)
+
 try{
     $global:currentIndex = $texbox.CurrentRow.Index;
     $currentCol = $texbox.CurrentCol.Index;
-    $currentRow = $texbox.FirstDisplayedScrollingRowIndex;    
-    $texbox.datasource = [collections.arraylist]$DATA;
+    $currentRow = $texbox.FirstDisplayedScrollingRowIndex;
+   if($DATA -eq $null)
+   {
+        $DATA;
+   }
+   elseif($DATA.GetType().Name -eq "PSCustomObject")
+   {
+
+        [Collections.ArrayList]$DATA = @($DATA |Where-Object {$_.Nombre -eq $DATA.Nombre} |Select-Object Nombre, PID, RAM, CPU);
+        write-host "este es DATA " $DATA;
+    }
+    else
+    {
+        $DATA =[Collections.ArrayList]$DATA;
+    write-host "este es DATA " $DATA;   
+    }
+    $texbox.datasource = $DATA;
+    write-host "este es mode " $global:mode  "este es la data "  $texbox.datasource;
     $texbox.CurrentCell = $texbox.Rows[$global:currentIndex].Cells[0];
     $texbox.FirstDisplayedScrollingRowIndex = $currentRow;  
     $texbox.update();
